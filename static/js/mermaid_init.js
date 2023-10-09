cfbc7c22e058671043e1c89a656d2c4282b677b2
Jay Looney <jay.m.looney@gmail.com>
2023-10-09 20:48:26 +0000

    Trying another solution for mermaid

---
+++ static/js/mermaid_init.js
@@ -0,0 +1,7 @@
+if (typeof mermaid !== "undefined") {
+  mermaid.initialize({
+    // I control the diagrams so I can relax the sandbox restrictions.
+    securityLevel: 'loose',
+    startOnLoad: true,
+  });
+}
